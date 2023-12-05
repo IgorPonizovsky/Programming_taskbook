40e1c7ed82aa9f69770a0f5f657ba3fe4d255210
Igor Ponizovsky <mr.Ponizovsky@gmail.com>
2023-12-05 16:05:01 +0300

    Task #8

---
+++ Begin/begin8.rs
@@ -0,0 +1,32 @@
+// Вт 05 дек 2023 15:52:49 MSK
+// Даны два числа 'a' и 'b'. 
+// Найти их среднее арифметическое: (a+b)/2. 
+
+use std::io; 
+
+fn main() { 
+    // Получение чисел от пользователя 
+    println!("Введите число a: "); 
+    let mut a = String::new(); 
+    io::stdin().read_line(&mut a)
+        .expect("Не удалось прочитать строку"); 
+    println!("Введите число b: "); 
+    let mut b = String::new(); 
+    io::stdin().read_line(&mut b)
+        .expect("Не удалось прочитать строку"); 
+    
+    // Приведение полученных данных из строк в числа 
+    let a: f32 = str_to_num(a);
+    let b: f32 = str_to_num(b);
+
+    // Выведение искомого значения на экран 
+    println!("Среднее арифметическое равно {}", (a+b)/2.0); 
+} 
+
+fn str_to_num(x: String) -> f32 { 
+    let x: f32 = match x.trim().parse() { 
+        Ok(num) => num, 
+        Err(_) => 0.0, 
+    }; 
+    return x;
+} 
