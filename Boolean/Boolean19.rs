c3df8ab7e9d52e873708585de51e84aaa9850cd5
Igor Ponizovsky <mr.Ponizovsky@gmail.com>
2023-12-11 17:09:36 +0300

    Task #19

---
+++ Boolean/Boolean19.rs
@@ -0,0 +1,50 @@
+// Пн 11 дек 2023 17:06:04 MSK
+// Проверить истинность высказывания: 
+// "Среди трёх данных целых чисел есть хотя бы одна пара взаимно противоположных".
+
+use std::io; 
+
+fn main() { 
+    // Получение чисел A, B и C от пользователя: 
+    println!("Введите целое число А: "); 
+    let a = read_user_line(); 
+    println!("Введите целое число B: "); 
+    let b = read_user_line(); 
+    println!("Введите целое число C: "); 
+    let c = read_user_line(); 
+
+    // Приведение полученных значений из строк в числа: 
+    let a: i32 = string_to_number(a); 
+    let b: i32 = string_to_number(b); 
+    let c: i32 = string_to_number(c); 
+
+    // Выведение логического значения: 
+    let t_f: bool; 
+    if (a / b == -1) | (a / c == -1) | (b / c == -1) { 
+        t_f = true; 
+    } else { 
+        t_f = false; 
+    }; 
+
+    // Выведение искомого результата: 
+    if t_f == true { 
+        println!("Среди трёх данных целых чисел есть хотя бы одна пара взаимно противоположных"); 
+    } else { 
+        println!("Среди трёх данных целых чисел нет ни одной пары взаимно противоположных"); 
+    }; 
+} 
+
+// Функция получения строки от пользователя 
+fn read_user_line() -> String { 
+    let mut some_string = String::new(); 
+    io::stdin().read_line(&mut some_string)
+        .expect("Не удалось прочитать строку"); 
+    some_string 
+} 
+
+// Функция приведения строки в число 
+fn string_to_number(some_string: String) -> i32 { 
+    let number: i32 = some_string.trim().parse()
+        .expect("Введите целые числа"); 
+    number 
+} 
